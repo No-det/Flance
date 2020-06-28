da1f17460298eda3119a53c9d10c86442fb96f2f
AbhinavRajesh <abhinavrajesh49@gmail.com>
2020-06-28 16:15:44 +0530

    Made the FreelancersHeader Header Responsive for Mobile and Tablet devices

--- src/Components/FreelancerHeader/index.jsx
+++ src/Components/FreelancerHeader/index.jsx
@@ -3,6 +3,12 @@ import "./index.css";
 import { Link } from "react-router-dom";
 
 const FreelancerHeader = () => {
+  function handleHamburgerClick() {
+    document.querySelector(".mobileNav").style.width = "200px";
+  }
+  const handleNavClose = () => {
+    document.querySelector(".mobileNav").style.width = "0px";
+  };
   return (
     <header>
       <div className="topNav">
@@ -17,19 +23,46 @@ const FreelancerHeader = () => {
           <ul>
             <li>
               <i class="fas fa-bell"></i>
-              Notification
+              <p>Notification</p>
             </li>
+            <hr />
             <li>
               <i class="fas fa-comment-alt"></i>
-              Messages
+              <p>Messages</p>
             </li>
             <li id="Profiledropdown">
               <div className="displayPicture"></div>
-              NAME
+              <p>NAME</p>
               <i class="fas fa-caret-down"></i>
             </li>
           </ul>
         </nav>
+        <div className="mobileNavWrapper">
+          <div className="hamburger" onClick={handleHamburgerClick}>
+            <i class="fas fa-bars fa-2x"></i>
+          </div>
+          <div className="mobileNav">
+            <i class="fas fa-times fa-2x" onClick={handleNavClose}></i>
+            <ul>
+              <li>
+                <div className="displayPicture"></div>
+                <p>NAME</p>
+              </li>
+              <li>
+                <i class="fas fa-bell"></i>
+                <p>Notification</p>
+              </li>
+              <li>
+                <i class="fas fa-comment-alt"></i>
+                <p>Messages</p>
+              </li>
+              <li>
+                <i class="fas fa-cog"></i>
+                <p>Settings</p>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
       <div className="bottomNav">
         <div className="navLeft">
